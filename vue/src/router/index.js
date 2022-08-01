502f23450e6d3832dacd2502a4f8e99f0314c2e9
Edmond GAKS <edmondgaks@gmail.com>
2022-08-01 11:54:38 +0200

    creating different routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,5 +1,21 @@
 import {createRouter, createWebHistory} from 'vue-router'
-const routes = [];
+const routes = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+];
 const router = createRouter({
     history: createWebHistory(),
     routes
