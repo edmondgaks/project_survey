e92203fe2d96a9c6e706630f3fe5c0e1e6f36ca7
Edmond GAKS <edmondgaks@gmail.com>
2022-08-06 13:26:05 +0200

    adding some states for auth

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,12 +4,14 @@ import Surveys from '../views/Surveys.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import store from '../store';
 
 const routes = [
     {
         path: '/',
         redirect: '/dashboard',
         component: DefaultLayout,
+        meta: {requiresAuth: true},
         children: [
             {path: '/dashboard',name: 'Dashboard',component: Dashboard},
             {path: '/survey',name: 'Surveys',component: Surveys}
@@ -30,5 +32,11 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
-
+router.beforeEach((to,from,next) => {
+    if(to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'Login'})
+    } else {
+        next();
+    }
+})
 export default router;
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,6 +1,20 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <h1>All surveys</h1>
+  <!-- <h1>All surveys</h1> -->
+  <header class="bg-white shadow">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+      </div>
+    </header>
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <div class="px-4 py-6 sm:px-0">
+          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">Content goes here</div>
+        </div>
+        <!-- /End replace -->
+      </div>
+    </main>
 </template>
 
 <script setup>
